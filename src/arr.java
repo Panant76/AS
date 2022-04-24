1dcb2c15e20f28c44989ce6264158d8d290eabcd
anton <panant@mail.ru>
2022-04-25 02:15:57 +0300

    массив нули в конец

--- src/arr.java
+++ src/arr.java
@@ -8,7 +8,6 @@ public class arr {
         int m = arr1.length -1;
         int i=0;
         while( i<arr1.length){
-        //for (int i = 0; i < arr.length; i++) {
             if (arr1[i] == 0) {
                 arr2[m] = 0;
                 m--;
@@ -22,32 +21,3 @@ public class arr {
 
 }
 
-   /* public static void moveNullToTheEnd(int[] arr) {
-
-        int[] temp = new int[arr.length];
-        int counter = 0;
-        int nullcounter = arr.length - 1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                temp[nullcounter] = 0;
-                nullcounter--;
-
-            } else {
-                temp[counter] = arr[i];
-                counter++;
-
-            }
-        }
-        arr = temp;
-
-        System.out.println(Arrays.toString(arr));
-
-    }
-
-
-    public static void main(String[] args) {
-        int[] test = new int[]{0, 2, 0, 34, 4,0,89,67,45,0};
-        moveNullToTheEnd(test);
-    }
-}
-*/
