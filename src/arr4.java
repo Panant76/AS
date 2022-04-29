a0d1ad2d5f9471ee788386a73cca45f4dd665021
anton <panant@mail.ru>
2022-04-29 08:52:28 +0300

    20 to 10 !!!

--- src/arr4.java
+++ src/arr4.java
@@ -3,17 +3,12 @@ public class arr4 {
     //4. Используя циклы, заполнить массив размером 10 чётными числами, которые лежат в промежутке от 20 до 1.
     public static void main(String[] args) {
         int[] array = new int[10];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (i + 1) * 2;
-        }
-        System.out.print(Arrays.toString(array));
+        int b=20;
+                for (int i = 0; i <array.length; i++) {
+                    array[i] = b;
+                b=b-2;
+        } System.out.print(Arrays.toString(array));
         System.out.println();
-        for (int i = 0; i < array.length / 2; i++) {
-            int temp = array[i];
-            array[i] = array[array.length - 1 - i];
-            array[array.length - 1 - i] = temp;
-        }
-        System.out.print(Arrays.toString(array));
     }
 
 }
