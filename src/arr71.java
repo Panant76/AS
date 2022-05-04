599b42c0eab80d0d84a180678cd756c429dec1cb
anton <panant@mail.ru>
2022-05-04 14:39:26 +0300

    Сортирока слов

--- src/arr71.java
+++ src/arr71.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class arr71 {
     public static void main(String[] args) {
         int[] array = new int[]{12, 83, 17, 31, 2, 7, 93, 76, 61, 22, 45, 98, 53, 16, 13};
-        int j=0;
+        int j = 0;
         for (int i = 0; i < array.length; i++) {
             int value = array[i];
             int n = i - 1;
@@ -17,11 +17,11 @@ public class arr71 {
             array[n + 1] = value;
         }
         for (int i = 0; i < array.length; i++) {
-            if (array[i]%2==0) {
-                int t=array[j];
+            if (array[i] % 2 == 0) {
+                int t = array[j];
                 array[j] = array[i];
                 array[i] = t;
-                j+=2;
+                j += 2;
             }
         }
         System.out.print(Arrays.toString(array));
