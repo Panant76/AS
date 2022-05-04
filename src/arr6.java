d7e363f945f2b51cc0f09527b67bf869fda76e5e
anton <panant@mail.ru>
2022-05-04 14:33:25 +0300

    Нули в конце с двумя массивами

--- src/arr6.java
+++ src/arr6.java
@@ -8,19 +8,17 @@ public class arr6 {
         int[] arr1 = new int[]{83, 17, 0, 0, 31, 0, 2, 7, 93, 0, 61, 0, 22};
         int[] arr2 = new int[arr1.length];
         int n = 0;
-        int m = arr1.length -1;
-        int i=0;
-        while( i<arr1.length){
+        int m = arr1.length - 1;
+        for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] == 0) {
                 arr2[m] = 0;
                 m--;
             } else {
                 arr2[n] = arr1[i];
                 n++;
-            }i++;
+            }
         }
         System.out.println(Arrays.toString(arr2));
     }
-
 }
 
