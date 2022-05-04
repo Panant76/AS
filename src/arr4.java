d9d4688a1a36a53390da8f429300bdef026809f8
anton <panant@mail.ru>
2022-05-04 14:14:50 +0300

    массив 10 от 20 до 1 четными

--- src/arr4.java
+++ src/arr4.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
+
 public class arr4 {
     //4. Используя циклы, заполнить массив размером 10 чётными числами, которые лежат в промежутке от 20 до 1.
     public static void main(String[] args) {
         int[] array = new int[10];
-        int b=20;
-                for (int i = 0; i <array.length; i++) {
-                    array[i] = b;
-                b=b-2;
-        } System.out.print(Arrays.toString(array));
+        int b = 20;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = b;
+            b = b - 2;
+        }
+        System.out.print(Arrays.toString(array));
         System.out.println();
     }
 
