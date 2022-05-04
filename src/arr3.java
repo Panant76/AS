e80845fa95da80320c890e89ac35921cb9feb7c6
anton <panant@mail.ru>
2022-05-04 14:12:28 +0300

    массив 10 от 1 до 20 четными

--- src/arr3.java
+++ src/arr3.java
@@ -1,11 +1,11 @@
 public class arr3 {
-//3. Используя циклы, заполнить массив размером 10 чётными числами, которые лежат в промежутке от 1 до 20.
-        public static void main(String[] args) {
-        int[] array=new int[10];
-        int i=0;
-        while(i<array.length) {
-            array[i]=(i+1)*2;
-            System.out.print(array[i]+",");
+    //3. Используя циклы, заполнить массив размером 10 чётными числами, которые лежат в промежутке от 1 до 20.
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        int i = 0;
+        while (i < array.length) {
+            array[i] = (i + 1) * 2;
+            System.out.print(array[i] + ",");
             i++;
         }
     }
