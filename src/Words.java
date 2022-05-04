2d35c65db695bd9f9df19c0842b48590638159a2
anton <panant@mail.ru>
2022-05-04 14:49:41 +0300

    Сортировка слов

---
+++ src/Words.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Words {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Lada", "Toyota", "Bentley", "Mercedes", "BMW",
+                "Wolkswagen", "Brabus", "Shkoda", "Scania", "Volvo", "Chevrolet", "Jaguar",
+                "Nissan", "Mitsubishi", "Mazda", "MAZ", "KAMAZ", "TATRA", "Fiat"};
+        Arrays.sort(arr, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
