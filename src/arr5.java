915f068cc5064d93587367d518ffa5aed17c56a8
Anton <anton_panoff@msn.com>
2023-12-11 16:37:40 +0300

    Доделан(справлен) arr5

--- src/arr5.java
+++ src/arr5.java
@@ -1,17 +1,17 @@
+import java.util.Arrays;
+
 public class arr5 {
     //5. Используя циклы, заполнить массив размером 20 числами, которые лежат в промежутке от 1 до 20.
 // Нечётные числа заменить на число 1
     public static void main(String[] args) {
         int[] array = new int[20];
-        int i = 0;
-        while (i < array.length) {
+
+        for (int i = 0; i < array.length; i++) {
             array[i] = i + 1;
-            if (i % 2 != 0)
+            if (array[i] % 2 != 0)
                 array[i] = 1;
-            //else array[i] = i + 1;
-            System.out.print(array[i] + ",");
-            i++;
         }
+        System.out.println(Arrays.toString(array));
     }
 }
 
